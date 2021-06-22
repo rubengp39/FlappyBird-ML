f198d2a9dd66349502bb44135c83250cbcc9a537
Rubén García Pedrejón <62309228+rubengp39@users.noreply.github.com>
2021-06-22 21:23:14 +0200

    Level System

--- FlappyBird/Assets/Scripts/Level.cs
+++ FlappyBird/Assets/Scripts/Level.cs
@@ -4,17 +4,178 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    private const float CAMERA_SIZE = 50f;
+    private const float PIPE_WIDTH = 8f;
+    private const float PIPE_HEAD_HIGHT = 4f;
+    private const float PIPE_MOVE_SPEED = 30f;
+    private const float PIPE_DESTROY_X_POSITION = -100f;
+    private const float PIPE_SPAWN_X_POSITION = 100f;
+
+    private List<Pipe> pipeList;
+    private int pipesSpawned;
+    private float pipeSpawnTimer;
+    private float pipeSpawnTimerMax;
+    private float gapSize;
+
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard,
+        Impossible,
+    }
+
+    private void Awake()
+    {
+        pipeList = new List<Pipe>();
+        pipeSpawnTimerMax = 1f;
+        SetDifficulty(Difficulty.Easy);
+    }
     private void Start()
     {
-        CreatePipe(0f, 0f);
-        CreatePipe(0f, 20f);
-    }
-    private void CreatePipe(float height, float x)
-    {
-        Transform pipeHead = Instantiate(GameAssets.GetInstance().pipeHead);
-        pipeHead.position = new Vector3(x, 0f);
 
+    }
+
+    private void Update()
+    {
+        HandlePipeMovement();
+        HandlePipeSpawning();
+    }
+    private void CreatePipe(float height, float x, bool createBottom)
+    {
+        //PipeHead
+        Transform pipeHead = Instantiate(GameAssets.GetInstance().pipeHead);
+        float pipeHeadYPosition;
+        if (createBottom) {
+            pipeHeadYPosition = height - PIPE_HEAD_HIGHT / 2 - CAMERA_SIZE;
+        } else
+        {
+            pipeHeadYPosition = -(height - PIPE_HEAD_HIGHT / 2 - CAMERA_SIZE);
+        }
+        pipeHead.position = new Vector3(x, pipeHeadYPosition);
+
+        //PipeBody
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pipeBody);
-        pipeBody.position = new Vector3(x, 0f);
+        float pipeBodyYPosition;
+        if (createBottom)
+        {
+            pipeBodyYPosition = -CAMERA_SIZE;
+        } else
+        {
+            pipeBodyYPosition = CAMERA_SIZE;
+            pipeBody.localScale = new Vector3(1, -1, 1);
+        }
+        pipeBody.position = new Vector3(x,pipeBodyYPosition);
+        
+
+        SpriteRenderer pipeBodySpriteRenderer = pipeBody.GetComponent<SpriteRenderer>();
+        pipeBodySpriteRenderer.size = new Vector2(PIPE_WIDTH , height);
+
+        BoxCollider2D pipeBodyBoxCollider = pipeBody.GetComponent<BoxCollider2D>();
+        pipeBodyBoxCollider.size = new Vector2(PIPE_WIDTH, height);
+        pipeBodyBoxCollider.offset = new Vector2(0f, height / 2);
+
+        Pipe pipe = new Pipe(pipeHead, pipeBody);
+        pipeList.Add(pipe);
+    }
+
+    private void CreateGapPipes(float gapY, float gapSize, float xPosition)
+    {
+        CreatePipe(gapY - gapSize / 2, xPosition, true);
+        CreatePipe(CAMERA_SIZE * 2 - gapY - gapSize / 2, xPosition, false);
+        pipesSpawned++;
+        SetDifficulty(GetDifficulty());
+    }
+
+    private void HandlePipeMovement()
+    {
+        for(int i = 0; i<pipeList.Count; i++)
+        {
+            Pipe pipe = pipeList[i];      
+            pipe.Move();
+            if(pipe.GetXPosition() < PIPE_DESTROY_X_POSITION)
+            {
+                pipe.DestroySelf();
+                pipeList.Remove(pipe);
+                i--;
+            }
+        }
+    }
+
+    private void HandlePipeSpawning()
+    {
+        pipeSpawnTimer -= Time.deltaTime;
+        if(pipeSpawnTimer < 0)
+        {
+            pipeSpawnTimer += pipeSpawnTimerMax;
+
+            float minHeight = gapSize / 2;
+            float heightEdgeLimit = 10f;
+            float totalHeight = CAMERA_SIZE * 2f;
+            float maxHeight = totalHeight - gapSize * .5f - heightEdgeLimit; 
+
+            float height = UnityEngine.Random.Range(minHeight, maxHeight);
+            CreateGapPipes(height, gapSize, PIPE_SPAWN_X_POSITION);
+        }
+    }
+
+    private void SetDifficulty(Difficulty difficulty)
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+                gapSize = 50f;
+                pipeSpawnTimer = 1.2f;
+                break;
+            case Difficulty.Medium:
+                gapSize = 40f;
+                pipeSpawnTimer = 1.1f;
+                break;
+            case Difficulty.Hard:
+                gapSize = 30f;
+                pipeSpawnTimer = 1f;
+                break;
+            case Difficulty.Impossible:
+                gapSize = 25f;
+                pipeSpawnTimer = .9f;
+                break;
+        }
+    }
+    private Difficulty GetDifficulty()
+    {
+        if (pipesSpawned >= 30) return Difficulty.Impossible;
+        if (pipesSpawned >= 20) return Difficulty.Hard;
+        if (pipesSpawned >= 10) return Difficulty.Medium;
+        return Difficulty.Easy;
+    }
+
+
+    private class Pipe
+    {
+        private Transform pipeHeadTransform;
+        private Transform pipeBodyTransform;
+
+        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform)
+        {
+            this.pipeHeadTransform = pipeHeadTransform;
+            this.pipeBodyTransform = pipeBodyTransform;
+        }
+
+        public void Move()
+        {
+            pipeHeadTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+            pipeBodyTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+        }
+
+        public float GetXPosition()
+        {
+            return pipeHeadTransform.position.x;
+        }
+
+        public void DestroySelf()
+        {
+            Destroy(pipeHeadTransform.gameObject);
+            Destroy(pipeBodyTransform.gameObject);
+        }
     }
 }
